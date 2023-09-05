26debc79e948513c9bfeac25879b6576b9cbfff8
ltansh <130187954+ltansh@users.noreply.github.com>
2023-09-05 15:27:18 -0700

    created UI interactions
    buttons hover state
    img scale on hover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 console.log("Your index.js file is loaded correctly!");
+
+$("#ux").on("hover", function(){
+    $("#ux").toggleClass(".displayImg");
+    if ($("#ux").hasClass(".displayImg")) {
+        ($(".displayImg").removeClass(".displayImg").addClass(".hiddenImg"));
+    }
+    else {($(".searchBar").css("height", "0vh"));
+        ($("#searchForm").css("opacity", "0"));
+        ($(".searchIcon").removeClass("fa-times").addClass("fa-search"));
+    }
+  })
