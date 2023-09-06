1c33e08b092e1f5189db9686a5b26392d55dcb9e
ltansh <130187954+ltansh@users.noreply.github.com>
2023-09-05 22:23:02 -0700

    updated button interaction

--- js/index.js
+++ js/index.js
@@ -1,12 +1,5 @@
 console.log("Your index.js file is loaded correctly!");
 
-$("#ux").on("hover", function(){
-    $("#ux").toggleClass(".displayImg");
-    if ($("#ux").hasClass(".displayImg")) {
-        ($(".displayImg").removeClass(".displayImg").addClass(".hiddenImg"));
-    }
-    else {($(".searchBar").css("height", "0vh"));
-        ($("#searchForm").css("opacity", "0"));
-        ($(".searchIcon").removeClass("fa-times").addClass("fa-search"));
-    }
+$(".btnContent").on("hover", function(){
+    $(".btnContent").attr('src', 'images/SmileyWink.png');
   })
