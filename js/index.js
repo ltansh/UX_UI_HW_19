6bdd6251c7dad609eee372bee15f941cba2cd82a
ltansh <130187954+ltansh@users.noreply.github.com>
2023-09-07 18:35:47 -0700

    updated img to svg

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 console.log("Your index.js file is loaded correctly!");
 
+$("#myID").on("click", function(){
+    console.log("You clicked on #myID");
+});
+
+$("#myID").on("click", function(){
+    $(".fadeMe").fadeToggle("slow");
+})
+
 $(".btnContent").on("hover", function(){
     $(".btnContent").attr('src', 'images/SmileyWink.png');
   })
